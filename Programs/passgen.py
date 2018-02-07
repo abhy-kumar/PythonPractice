68b83de0f0b7cbce80a3c188774110580c97e4d7
Abhishek Kumar <35186422+abhy-kumar@users.noreply.github.com>
2018-02-07 16:38:45 +0530

    Minor corrections and updated documentation.

--- Programs/passgen.py
+++ Programs/passgen.py
@@ -7,16 +7,16 @@ n = 8
 pw = []
 
 s = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-     'xy', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+     'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
      'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-# Character containing Array
+# Character containing Array. You may modify this if you need some special characters in there.
 
 for i in range(0, n):
     pw.append(s[random.randrange(len(s))])
 
 fpw = ''.join((pw))
 print(fpw)
-print("The password is already generated to clipboard.")
+print("\nThe password is already copied to clipboard.")
 
 # copy directly to clipboard if a password is generated.
 df = pd.DataFrame([fpw])
