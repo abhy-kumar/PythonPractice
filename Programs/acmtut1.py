d19a778ef9812040dd69dd9bac7d686fff69db90
Abhishek Kumar <35186422+abhy-kumar@users.noreply.github.com>
2018-05-25 01:28:05 +0530

    Pratice for ACM Summer Challenge

---
+++ Programs/acmtut1.py
@@ -0,0 +1,12 @@
+for i in range(0, 3):
+    print("\n")
+    for j in range(0, i + 1):
+        print(" ", end="")
+    for k in range(0, i + 1):
+        print("* ", end="")
+for i in range(2, 0, -1):
+    print("\n")
+    for j in range(0, i):
+        print(" ", end="")
+    for k in range(0, i):
+        print("* ", end="")
